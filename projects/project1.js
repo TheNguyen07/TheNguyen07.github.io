3e10133efd00d7d4b7af623558e89e1e462ace7d
TheNguyen07 <powerfulminon@outlook.com>
2024-05-13 20:34:02 -0500

    phone test

---
+++ projects/project1.js
@@ -0,0 +1,8 @@
+const stop = ['bruh', 'why?', 'stop that'];
+const counter = 0;
+function no() {
+    alert(counter);
+    // if (counter == 3){
+    //     document.getElementById('butt2').style.display = 'hidden';
+    // }
+}
