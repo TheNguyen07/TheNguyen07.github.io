dca57f4a05d055f246dc4c829ab7e3b8028a7c4e
TheNguyen07 <powerfulminon@outlook.com>
2024-05-13 21:10:52 -0500

    almost done with p1

--- projects/project1.js
+++ projects/project1.js
@@ -1,8 +1,23 @@
-const stop = ['bruh', 'why?', 'stop that'];
-const counter = 0;
-function no() {
-    alert(counter);
-    // if (counter == 3){
-    //     document.getElementById('butt2').style.display = 'hidden';
-    // }
-}
+let noButton = document.getElementById('butt2');
+let yesButton = document.getElementById('butt1');
+document.getElementById('test').style.visibility = 'hidden';
+
+let c = 0;
+let stop = ['bruh', 'why?', 'stop that'];
+noButton.addEventListener('click', () => {
+    alert(stop[c]);
+    c++;
+    if(c==3){
+        document.getElementById('butt2').style.display = 'none';
+        alert('HA you have no choice now but to go on a date with me :]')
+    }
+})
+
+yesButton.addEventListener('click', () =>{
+    document.getElementById('butt1').style.display = 'none';
+    document.getElementById('butt2').style.display = 'none';
+    document.getElementById('asking').style.display = 'none';
+    document.getElementsByClassName('heart')[0].style.display = 'none';
+    document.getElementById('test').style.visibility = 'visible';
+})
+
